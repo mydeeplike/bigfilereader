88a4c3e5e7c1d065395a2d0e96c2b6e40c012f34
hm <hm@johnwick.io>
2019-05-31 13:13:48 +0800

    demo code

--- bigfilereader_test.go
+++ bigfilereader_test.go
@@ -10,7 +10,39 @@ import (
 	"testing"
 )
 
+// 第 1 种方式：推荐
 func TestScan1(t *testing.T) {
+	big, _ := New("1.log")
+	defer big.Close()
+
+	big.LineCallback = func(b []byte) []byte {
+		s := string(b)
+		arr := strings.Split(s, ",")
+		t := fmt.Sprintf(`('%v','%v'),`, arr[0], arr[1])
+
+		/* 对于大数据，这种比字符串相加快很多！
+		buf := bytes.NewBuffer([100]byte{}[:])
+		buf.WriteString("xxx")
+		buf.Bytes()
+		*/
+		return []byte(t)
+	}
+
+	big.MergeCallback = func(mergeBytes []byte) {
+		mergeBytes = bytes.TrimRight(mergeBytes, ",")
+		sql1 := fmt.Sprintf("INSERT INTO xxx (id, name) VALUES %v", string(mergeBytes))
+		fmt.Printf(sql1)
+	}
+
+	big.Run(1, 100000000, 50)
+
+	// 4 : 线程数，100000000: 每次读取的文件块大小，2000000：每次合并的大小
+	// big.Run(4, 100000000, 2000000)
+}
+
+
+// 第 2 种方式：不推荐
+func TestScan2(t *testing.T) {
 	big, _ := New("1.log")
 	defer big.Close()
 
@@ -45,33 +77,8 @@ func TestScan1(t *testing.T) {
 	t.Log("done")
 }
 
-func TestScan2(t *testing.T) {
-	big, _ := New("1.log")
-	defer big.Close()
 
-	big.LineCallback = func(b []byte) []byte {
-		s := string(b)
-		arr := strings.Split(s, ",")
-		t := fmt.Sprintf(`('%v','%v'),`, arr[0], arr[1])
-
-		/* 对于大数据，这种比字符串相加快很多！
-		buf := bytes.NewBuffer([100]byte{}[:])
-		buf.WriteString("xxx")
-		buf.Bytes()
-		*/
-		return []byte(t)
-	}
-
-	big.MergeCallback = func(mergeBytes []byte) {
-		mergeBytes = bytes.TrimRight(mergeBytes, ",")
-		sql1 := fmt.Sprintf("INSERT INTO xxx (id, name) VALUES %v", string(mergeBytes))
-		fmt.Printf(sql1)
-	}
-
-	big.Run(1, 100000000, 50)
-}
-
-// 单线程读取: 效率低下
+// 第 3 种方式：单线程读取: 效率低下，不推荐
 func TestScan3(t *testing.T) {
 	obj, _ := New("1.log")
 	for {
@@ -81,9 +88,4 @@ func TestScan3(t *testing.T) {
 		}
 		fmt.Println(s)
 	}
-}
-
-func Test1(t *testing.T) {
-	s := "abc"
-	println(s[0 : len(s)-1])
 }
